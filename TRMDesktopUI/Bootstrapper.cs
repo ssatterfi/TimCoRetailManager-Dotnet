d4555a5971f1011cc91d94568c5cb7a94b5a3f78
Scott Satterfield <ssatterf@gmail.com>
2020-07-10 13:25:14 -0400

    TRM Desktop UI Setup
    Add TRM Desktop WPF project

---
+++ TRMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    public class Bootstrapper : Caliburn.Micro.BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+    }
+}
